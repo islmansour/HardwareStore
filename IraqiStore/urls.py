c4dc705625c82cebb7cbd0ff15e308b25afe9f97
islmansour <67031389+islmansour@users.noreply.github.com>
2022-05-06 15:21:50 +0300

    REST API functions and CRUD on all entities - without delete

--- IraqiStore/urls.py
+++ IraqiStore/urls.py
@@ -2,14 +2,28 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('product_list', views.get_data),
+    path('product_list', views.get_products),
     path('inventory_list', views.get_inventory),
     path('contact_list', views.get_contacts),
     path('account_list', views.get_accounts),
-    path('quote_list', views.get_quote),
+    path('quote_list', views.get_quotes),
     path('quote_item_list', views.get_quote_item),
-    path('order_list', views.get_order),
+    path('order_list', views.get_orders),
     path('order_item_liat', views.get_order_item),
+    path('single_product/<str:pk>', views.get_single_product),
+    path('single_inventory/<str:pk>', views.get_single_inventory),
+    path('single_contact/<str:pk>', views.get_single_contact),
+    path('single_account/<str:pk>', views.get_single_account),
+    path('single_quote/<str:pk>', views.get_single_quote),
+    path('single_order/<str:pk>', views.get_single_order),
+    path('upsert_product/<str:pk>', views.upsert_product),
+    path('upsert_inventory/<str:pk>', views.upsert_inventory),
+    path('upsert_contact/<str:pk>', views.upsert_contact),
+    path('upsert_account/<str:pk>', views.upsert_account),
+    path('upsert_quote/<str:pk>', views.upsert_quote),
+    path('upsert_quote_item/<str:pk>', views.upsert_quote_item),
+    path('upsert_order/<str:pk>', views.upsert_order),
+    path('upsert_order_item/<str:pk>', views.upsert_order_item),
 
 
 ]
--- IraqiStore/views.py
+++ IraqiStore/views.py
@@ -13,9 +13,34 @@ def iraqi_view(request):
 
 
 @api_view(['GET'])
-def get_data(request):
+def get_products(request):
     products = Product.objects.all()
     serializer = productSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_single_product(request, pk):
+    products = Product.objects.get(id=pk)
+    serializer = productSerializer(products, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def upsert_product(request, pk):
+    try:
+        record = Product.objects.get(id=pk)
+        serializer = productSerializer(instance=record, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+    except:
+        try:
+            serializer = productSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+        except:
+            return('Error while upserting a product.')
+
     return Response(serializer.data)
 
 
@@ -27,9 +52,59 @@ def get_inventory(request):
 
 
 @api_view(['GET'])
+def get_single_inventory(request, pk):
+    inventory = Inventory.objects.get(id=pk)
+    serializer = inventorySerializer(inventory, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def upsert_inventory(request, pk):
+    try:
+        record = Inventory.objects.get(id=pk)
+        serializer = inventorySerializer(instance=record, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+    except:
+        try:
+            serializer = inventorySerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+        except:
+            return('Error while upserting a inventory.')
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
 def get_contacts(request):
     contacts = Contact.objects.all()
     serializer = contactSerializer(contacts, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_single_contact(request, pk):
+    contacts = Contact.objects.get(id=pk)
+    serializer = contactSerializer(contacts, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def upsert_contact(request, pk):
+    try:
+        record = Contact.objects.get(id=pk)
+        serializer = contactSerializer(instance=record, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+    except:
+        try:
+            serializer = contactSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+        except:
+            return('Error while upserting a contact.')
+
     return Response(serializer.data)
 
 
@@ -41,9 +116,59 @@ def get_accounts(request):
 
 
 @api_view(['GET'])
-def get_quote(request):
+def get_single_account(request, pk):
+    accounts = Account.objects.all(id=pk)
+    serializer = accountSerializer(accounts, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def upsert_account(request, pk):
+    try:
+        record = Account.objects.get(id=pk)
+        serializer = accountSerializer(instance=record, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+    except:
+        try:
+            serializer = accountSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+        except:
+            return('Error while upserting an account.')
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_quotes(request):
     quotes = Quote.objects.all()
     serializer = quoteSerializer(quotes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_single_quote(request, pk):
+    quotes = Quote.objects.get(id=pk)
+    serializer = quoteSerializer(quotes, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def upsert_quote(request, pk):
+    try:
+        record = Quote.objects.get(id=pk)
+        serializer = quoteSerializer(instance=record, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+    except:
+        try:
+            serializer = quoteSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+        except:
+            return('Error while upserting an quote.')
+
     return Response(serializer.data)
 
 
@@ -54,10 +179,53 @@ def get_quote_item(request):
     return Response(serializer.data)
 
 
+@api_view(['POST'])
+def upsert_quote_item(request, pk):
+    try:
+        record = QutoeItem.objects.get(id=pk)
+        serializer = quoteItemSerializer(instance=record, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+    except:
+        try:
+            serializer = quoteItemSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+        except:
+            return('Error while upserting a quote item.')
+
+    return Response(serializer.data)
+
+
 @api_view(['GET'])
-def get_order(request):
+def get_orders(request):
     orders = Order.objects.all()
     serializer = orderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_single_order(request, pk):
+    orders = Order.objects.get(id=pk)
+    serializer = orderSerializer(orders, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def upsert_order(request, pk):
+    try:
+        record = Order.objects.get(id=pk)
+        serializer = orderSerializer(instance=record, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+    except:
+        try:
+            serializer = orderSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+        except:
+            return('Error while upserting an order.')
+
     return Response(serializer.data)
 
 
@@ -65,4 +233,22 @@ def get_order(request):
 def get_order_item(request):
     items = OrderItem.objects.all()
     serializer = orderItemSerializer(items, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def upsert_order_item(request, pk):
+    try:
+        record = OrderItem.objects.get(id=pk)
+        serializer = orderItemSerializer(instance=record, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+    except:
+        try:
+            serializer = orderItemSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+        except:
+            return('Error while upserting an order item.')
+
     return Response(serializer.data)
