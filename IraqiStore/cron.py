66432829dfc6bc48fc8182104f4228ee9fcb1a12
islmansour <67031389+islmansour@users.noreply.github.com>
2022-08-19 21:44:16 +0300

    fcm fix

--- IraqiStore/cron.py
+++ IraqiStore/cron.py
@@ -40,3 +40,4 @@ class news_notify(CronJobBase):
 def my_scheduled_job():
     tokens = ["dwyJ-fW-Lkteo3nSn8mdSu:APA91bG2yzQJeEtXJU8tM4utv91Xse_2w4IQ86Gz3v68QQ0GXTCZFgruBxIEDS2BVSBfiZ_NNf__U9Rdk63SRV7OssFeSiDo_nFub9cuGHcjRKZmn8yZFvSrXQHqENaiZk8W2k5slVwi"]
     sendPush("Hi", "This is my next msg", tokens)
+    raise Exception("testing cron error")
